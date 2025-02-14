bab7c5fb19974e0445c88f7a7b6b3e532135de94
Kevin Pan <kevinrpan@gmail.com>
2025-02-13 19:20:51 -0500

    Initial Commit

---
+++ Airtime/AirtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirtimeApp.swift
+//  Airtime
+//
+//  Created by Kevin Pan on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AirtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirtimeTests/AirtimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirtimeTests.swift
+//  AirtimeTests
+//
+//  Created by Kevin Pan on 2/13/25.
+//
+
+import Testing
+@testable import Airtime
+
+struct AirtimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
